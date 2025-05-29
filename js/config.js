e235ad3116e4cf892e44d914e4290518d04ac356
initTest <69097591+initTest@users.noreply.github.com>
2025-05-28 22:33:36 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_wv5GEhA2A', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4k2r449gricoo5inu2rrfksv4e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '1nlfpc692kg28rkvoa29n4m7p77jm3vf6nbnkbps4899v1iq6s9m', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
