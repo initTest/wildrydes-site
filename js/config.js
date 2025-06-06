b1aa17e9d8239b216694d78cf622ebb5ec7694bb
initTest <69097591+initTest@users.noreply.github.com>
2025-06-05 23:31:16 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: process.env.REACT_APP_COGNITO_USER_POOL_ID ,
-        userPoolClientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
-        region: process.env.REACT_APP_AWS_REGION
+        userPoolId: window.REACT_APP_COGNITO_USER_POOL_ID ,
+        userPoolClientId: window.REACT_APP_COGNITO_CLIENT_ID,
+        region: window.REACT_APP_AWS_REGION
     },
     api: {
-        invokeUrl: process.env.REACT_APP_API_INVOKE_URL
+        invokeUrl: window.REACT_APP_API_INVOKE_URL
     }
 };
